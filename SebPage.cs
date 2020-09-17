1328a30f56d978f37831317712ff7dc9bc06bcd8
Marius <mariusskikas@gmail.com>
2020-09-17 13:53:10 +0300

    My Seb Page Test

---
+++ SebPage.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestLeson1.NamuDarbai
+{
+    class SebPage
+    {
+        private static IWebDriver _driver;
+
+        private IWebElement _incomeInput => _driver.FindElement(By.CssSelector("#income"));
+        private SelectElement _cityDropDown => new SelectElement(_driver.FindElement(By.Id("city")));
+        private IWebElement _button => _driver.FindElement(By.Id("calculate"));
+        private IWebElement _actualResult => _driver.FindElement(By.CssSelector("#mortgageCalculatorStep2 > div.row > div > div:nth-child(5) > div > span > strong"));
+
+        public SebPage(IWebDriver webdriver)
+        {
+            _driver = webdriver;
+        }
+
+        public void InsertIncome(string income)
+        {
+            _incomeInput.Clear();
+            _incomeInput.SendKeys(income);
+        }
+
+        public void SelectDropDownValue(string city)
+        {
+            _cityDropDown.SelectByText(city);
+        }
+        public void ClickButton()
+        {
+            _button.Click();
+        }
+
+        public void CheckResult()
+        {
+            Assert.IsTrue(CompareResult(), "Suma mazesne nei 75000");
+        }
+
+        private bool CompareResult()
+        {
+            string actualResult = _actualResult.Text;
+            actualResult = actualResult.Replace(" ", "");
+
+            int result = Convert.ToInt32(actualResult);
+            
+            if (result > 75000)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ SebTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoTestLeson1.NamuDarbai;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestLeson1.Test
+{
+    class SebTest
+    {
+        private static IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.seb.lt/privatiems/kreditai-ir-lizingas/kreditai/busto-kreditas-0#1");
+            
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            IWebElement popUp = _driver.FindElement(By.CssSelector("body > div.seb-cookie-consent.seb-cookiemessage > div > div:nth-child(4) > ul > li:nth-child(1) > a > span"));
+            wait.Until(d => d.FindElement(By.CssSelector("body > div.seb-cookie-consent.seb-cookiemessage > div > div:nth-child(4) > ul > li:nth-child(1) > a > span")).Displayed);
+            wait.Until(d => popUp.Displayed);
+            popUp.Click();
+            _driver.SwitchTo().Frame(0);
+        }
+
+        [Test]
+
+        public static void TestSeb()
+        {
+            SebPage page = new SebPage(_driver);
+            page.InsertIncome("1000");
+            page.SelectDropDownValue("Klaipėda");
+            page.ClickButton();
+            page.CheckResult();
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            _driver.Quit();
+        }
+
+    }
+}
